f6a4efa5fbce434f4784f7e10d8bb6b88e3fd5d7
Akyoo501 <143004721+Akyoo501@users.noreply.github.com>
2024-12-17 20:11:47 +0300

    Add files via upload

---
+++ How to create a class.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+class Student{
+	public:               //Access spacefier
+		int no;          // Attribute (int variable)
+		string name;    //  Attribute (string variable)
+};
+
